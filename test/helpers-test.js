73f77f5e6bbf02505eeb5f914c7e0f19783886bd
bucko13 <bucko.perley@gmail.com>
2018-12-10 15:43:26 -0800

    fail test 2

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -5,6 +5,9 @@ const Config = require('bcfg');
 const fs = require('bfile');
 const Blgr = require('blgr');
 
+const { Mocha } = require('bmocha');
+const mocha = new Mocha(process.stdout);
+
 const {
   loadConfig,
   createClientConfig,
@@ -19,78 +22,84 @@ process.env.BPANEL_CLIENTS_DIR = 'test_clients';
 const { BPANEL_PREFIX, BPANEL_CLIENTS_DIR } = process.env;
 const clientsDirPath = resolve(BPANEL_PREFIX, BPANEL_CLIENTS_DIR);
 
-describe('configHelpers', () => {
-  let apiKey, ports, options, id, config, logger;
+(async () => {
+  const code = await mocha.run(() => {
+    describe('configHelpers', () => {
+      let apiKey, ports, options, id, config, logger;
 
-  before(async () => {
-    logger = new Blgr({ level: 'none' });
-    id = 'test';
-    apiKey = 'foo';
-    ports = {
-      p2p: 49331,
-      node: 49332,
-      wallet: 49333,
-    };
-    await logger.open();
-  });
+      before(async () => {
+        logger = new Blgr({ level: 'none' });
+        id = 'test';
+        apiKey = 'foo';
+        ports = {
+          p2p: 49331,
+          node: 49332,
+          wallet: 49333,
+        };
+        await logger.open();
+      });
 
-  after(async function() {
-    if (fs.existsSync(testDir)) {
-      fs.rimrafSync(testDir);
-    }
-    await logger.close();
-  });
+      after(async function() {
+        if (fs.existsSync(testDir)) {
+          fs.rimrafSync(testDir);
+        }
+        await logger.close();
+      });
 
-  beforeEach(() => {
-    config = new Config(id);
-    options = {
-      id,
-      chain: 'bitcoin',
-      port: ports.node,
-      network: 'regtest',
-      apiKey,
-      'wallet-port': ports.wallet,
-      multisigWallet: false,
-    };
-  });
+      beforeEach(() => {
+        config = new Config(id);
+        options = {
+          id,
+          chain: 'bitcoin',
+          port: ports.node,
+          network: 'regtest',
+          apiKey,
+          'wallet-port': ports.wallet,
+          multisigWallet: false,
+        };
+      });
 
-  describe('createClientConfig', () => {
-    it('should accept options object or a bcfg object', async () => {
-      assert(false, 'This failed!');
-      await createClientConfig(id, options, logger);
-      config.inject(options);
-      await createClientConfig(id, config, logger);
+      describe('createClientConfig', () => {
+        it('should accept options object or a bcfg object', async () => {
+          assert(false, 'This failed!');
+          await createClientConfig(id, options, logger);
+          config.inject(options);
+          await createClientConfig(id, config, logger);
+        });
+
+        it('should create new config file in clients directory with correct configs', async () => {
+          config.inject(options);
+          await createClientConfig(id, options, logger);
+          const { BPANEL_PREFIX, BPANEL_CLIENTS_DIR } = process.env;
+          const clientPath = resolve(
+            BPANEL_PREFIX,
+            BPANEL_CLIENTS_DIR,
+            `${id}.conf`
+          );
+
+          assert(
+            fs.existsSync(clientPath),
+            'Could not find config file at expected path'
+          );
+
+          const loadedConfigs = loadConfig(id, { id, prefix: clientsDirPath });
+          loadedConfigs.open(`${id}.conf`);
+        });
+      });
+
+      describe('deleteConfig', () => {
+        it('should remove a config file', async () => {
+          await createClientConfig(id, options, logger);
+          let config = getConfig(id);
+          assert(config, 'Config did not exist before testing deletion');
+          deleteConfig(id, logger);
+          const path = resolve(config.prefix, `${config.str('id')}.conf`);
+          const exists = fs.existsSync(path);
+          assert(!exists, 'Config should not exist after deletion');
+        });
+      });
     });
-
-    it('should create new config file in clients directory with correct configs', async () => {
-      config.inject(options);
-      await createClientConfig(id, options, logger);
-      const { BPANEL_PREFIX, BPANEL_CLIENTS_DIR } = process.env;
-      const clientPath = resolve(
-        BPANEL_PREFIX,
-        BPANEL_CLIENTS_DIR,
-        `${id}.conf`
-      );
-
-      assert(
-        fs.existsSync(clientPath),
-        'Could not find config file at expected path'
-      );
-
-      const loadedConfigs = loadConfig(id, { id, prefix: clientsDirPath });
-      loadedConfigs.open(`${id}.conf`);
-    });
   });
 
-  describe('deleteConfig', () => {
-    it('should remove a config file', async () => {
-      await createClientConfig(id, options, logger);
-      let config = getConfig(id);
-      assert(config, 'Config did not exist before testing deletion');
-      deleteConfig(id, logger);
-      const path = resolve(config.prefix, `${config.str('id')}.conf`);
-      const exists = fs.existsSync(path);
-      assert(!exists, 'Config should not exist after deletion');
-    });
-  });
-});
+  if (code !== 0) process.exit(code);
+})();
